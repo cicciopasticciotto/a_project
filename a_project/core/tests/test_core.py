13efd1d8fc2e26b0dfc0172a824edd698b287f55
Ciccio Pasticcio <41234180+cicciopasticciotto@users.noreply.github.com>
2018-07-16 00:19:13 +0200

    Update test_core.py

--- a_project/core/tests/test_core.py
+++ a_project/core/tests/test_core.py
@@ -1,7 +1,11 @@
 import unittest
-from a_project.core.core import a_sleep_function
+from a_project.core.core import a_sleep_function, plot_something
 
 
 class TestSleep(unittest.TestCase):
     def test_sleep(self):
         a_sleep_function()
+
+    def test_plot(self):
+        plot_something()
+        
